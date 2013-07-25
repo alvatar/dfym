a448c8a4182d57c0bb0f34db304eb0ee99f3c393
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2013-07-25 15:01:31 +0200

    read files from directory using GLib

--- src/bin/main.c
+++ src/bin/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
+
+#include <glib.h>
+#include <glib/gstdio.h>
+
 #include "dfym_base.h"
 
-int main() {
+int main(int argc, char **argv)
+{
   printf("Welcome to DFYM\n");
+
+  /* Read directory files */
+  GDir *dir;
+  GError *error;
+  const gchar *filename;
+
+  dir = g_dir_open(argv[1], 0, &error);
+  while ( (filename = g_dir_read_name(dir)) )
+    {
+      printf("%s\n", filename);
+    }
+
   return dfym_fun();
 }
 
