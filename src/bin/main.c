388cceb0cc76d346064dcdadaff722d9ea4ae24f
Álvaro Castro-Castilla <alvcastro@yahoo.es>
2013-07-25 15:29:09 +0200

    added commands and options

--- src/bin/main.c
+++ src/bin/main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <getopt.h>
+#include <string.h>
 
 #include <glib.h>
 #include <glib/gstdio.h>
@@ -7,9 +9,8 @@
 
 int main(int argc, char **argv)
 {
-  printf("Welcome to DFYM\n");
-
   /* Read directory files */
+  /*
   GDir *dir;
   GError *error;
   const gchar *filename;
@@ -19,6 +20,44 @@ int main(int argc, char **argv)
     {
       printf("%s\n", filename);
     }
+    */
+
+  /* String compare */
+  if (!strcmp("tag", argv[1]))
+    {
+      printf("TAG\n");
+    }
+  else if (!strcmp("search", argv[1]))
+    {
+      printf("SEARCH WITH TAGS\n");
+    }
+  else if (!strcmp("show", argv[1]))
+    {
+      printf("SHOW TAGS\n");
+    }
+  else
+    {
+      printf("Please provide a command\n");
+    }
+
+  /* Argument parsing */
+
+  int opt;
+  int aflag = 0;
+
+  while ( (opt = getopt(argc, argv, "abc:")) != -1)
+    {
+      switch (opt)
+        {
+        case 'a':
+          aflag = 1;
+          break;
+        default:
+          break;
+        }
+    }
+
+  printf("Flags: a=%i\n", aflag);
 
   return dfym_fun();
 }
